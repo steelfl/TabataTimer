c4b8fdc168e6ac3942c98d184dd12b6f0d1669ee
steelfl <62485317+steelfl@users.noreply.github.com>
2021-04-11 13:30:38 +0300

    added network

--- app/src/main/java/com/clouweth/tabatatimer/MainActivityTg.java
+++ app/src/main/java/com/clouweth/tabatatimer/MainActivityTg.java
@@ -2,13 +2,63 @@ package com.clouweth.tabatatimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivityTg extends AppCompatActivity {
+    private final String urlAddress = "https://pesni.guru/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tg);
+        TextView textView = findViewById(R.id.textField);
+        Log.i("internet", urlAddress);
+        DownloadHelper downloadHelper = new DownloadHelper();
+        try {
+            String result = downloadHelper.execute(urlAddress).get();
+            Log.i("internet", result);
+            textView.setText(result);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    private static class DownloadHelper extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder stringBuilder = new StringBuilder();
+            HttpURLConnection httpURLConnection = null;
+            try {
+                URL url = new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line = bufferedReader.readLine();
+                while (line != null) {
+                    stringBuilder.append(line);
+                    line = bufferedReader.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+            }
+            return stringBuilder.toString();
+        }
     }
 }
